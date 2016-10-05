038f09061fa1e4acf01f8f38627f603804da449d
Martin Marušiak <xmarus07@stud.fit.vutbr.cz>
2016-10-05 16:39:36 +0200

    adding names

--- tokens_table.h
+++ tokens_table.h
@@ -1,45 +1,45 @@
 #ifndef TOKENS_TABLE_H
 #define TOKENS_TABLE_H
 
-#define LEXICAL_ERROR 999
+enum tokens{   
+        ID,
+        KEYWORD,
+        NUMBER,
+        DOUBLE,
+        ADD,
+        SUB,
+        MUL,
+        DIV,
+        SEMICOLON,
 
-#define ID 0
-#define KEYWORD 1
-#define NUMBER 2
-#define DOUBLE 3
-#define ADD 4
-#define SUB 5
-#define MUL 6
-#define DIV 7
-#define SEMICOLON 8
+        LEFT_CURVED_BRACKET,
+        RIGHT_CURVED_BRACKET,
+        LEFT_ROUNDED_BRACKET,
+        RIGHT_ROUNDED_BRACKET,
 
-#define LEFT_CURVED_BRACKET 9
-#define RIGHT_CURVED_BRACKET 10
-#define LEFT_ROUNDED_BRACKET 11
-#define RIGHT_ROUNDED_BRACKET 12
+        ASSIGN,
 
-#define ASSIGN 13
+        LINE_COMMENT,
+        BLOCK_COMMENT,
 
-#define LINE_COMMENT 14
-#define BLOCK_COMMENT 15
+        LOGICAL_AND,
+        LOGICAL_OR,
+        COMMA,
+        NEG,
+        SIMPLE_QUOTE,
+        STRING_LITERAL,
 
-#define LOGICAL_AND 16
-#define LOGICAL_OR 17
+        COLON,
+        NOT_EQUAL,
+        LESS_EQUAL,
+        LESS,
+        GREATER_EQUAL,
+        GREATER,
 
-#define COMMA 18
-#define NEG 19
-#define SIMPLE_QUOTE 20
-#define STRING_LITERAL 21
+        EQUAL,
+        SPECIAL_ID, /* plne kvatifikovany */
+        LEXICAL_ERROR
 
-#define COLON 24
-#define NOT_EQUAL 25
-#define LESS_EQUAL 26
-#define LESS 27
-#define GREATER_EQUAL 28
-#define GREATER 29
-
-#define EQUAL 33
-#define SPECIAL_ID 34 /* plne kvatifikovany */
-
+};
 
 #endif // TOKENS_TABLE_H
