433355e8f329c71c3cbbc3eed3aa54867740f2d2
Dávid Bolvanský <xbolva00@stud.fit.vutbr.cz>
2016-10-11 18:51:11 +0200

    Params list AST

--- src/parser.c
+++ src/parser.c
@@ -3,6 +3,7 @@
 
 void parse_class(FILE *file); // will return error code
 void parse_method(FILE *file);
+void parse_param_list(FILE *file);
 
 void parse(FILE *file) {
         token t;
@@ -42,7 +43,7 @@ void parse_class(FILE *file) {
                 type = get_next_token(&t, file);
 
                 if (type == RIGHT_CURVED_BRACKET) {
-                        return;
+                        break; // Koniec classy
 
                 }
 
@@ -53,48 +54,86 @@ void parse_class(FILE *file) {
 
                 type = get_next_token(&t, file);
 
-                if (type != VOID && type != INTEGER && type != DOUBLE) {
-                        printf("Bad return type for method\n");
+                if (type == VOID) {
+                        type = get_next_token(&t, file);
+                        if (type != ID) {
+                                printf("No METHOD ID\n");
+                        }
+                        parse_method(file);
+                }
+
+                if (type != INT && type !=  DOUBLE) {
+                        printf("Bad return type / data type\n");
                         break;
                 }
 
-                parse_method(file);
-        }
+                type = get_next_token(&t, file);
+                if (type != ID) {
+                        printf("No METHOD ID\n");
+                }
 
-        //printf("token %s\n", token_names[type]);
+                type = get_next_token(&t, file);
+                if (type != ASSIGN) {
+                        parse_method(file);
+                } else {
+
+                        //parse_expr(file);
+                        //printf("hi\n");
+                        break;
+                }
+        }
 }
 
 void parse_method(FILE *file) {
         token t;
+        parse_param_list(file); // foo(params)
+
         int type = get_next_token(&t, file);
-        if (type != ID) {
-                printf("No METHOD ID\n");
-        }
-
-        type = get_next_token(&t, file);
-        if (type != LEFT_ROUNDED_BRACKET) {
-                printf("Missing (\n");
-        }
-
-        type = get_next_token(&t, file);
-        if (type != RIGHT_ROUNDED_BRACKET) {
-                printf("Missing )\n");
-        }
-        type = get_next_token(&t, file);
         if (type != LEFT_CURVED_BRACKET) {
                 printf("Missing {\n");
         }
 
-        // ignore body
+        // ignore body for now
 
         while (type != RIGHT_CURVED_BRACKET) {
                 type = get_next_token(&t, file);
         }
 
-        //
-
         if (type != RIGHT_CURVED_BRACKET) {
                 printf("Missing }\n");
+        }
+
+}
+
+void parse_param_list(FILE *file) {
+        token t;
+        int type = get_next_token(&t, file);
+        if (type != LEFT_ROUNDED_BRACKET) {
+                printf("Missing (\n");
+        }
+
+        while (1) {
+
+                type = get_next_token(&t, file);
+                if (type != INT && type != STRING && type != DOUBLE) {
+                        printf("LP Missing data type\n");
+                }
+
+                type = get_next_token(&t, file);
+                if (type != ID) {
+                        printf("Missing param ID\n");
+                }
+
+                type = get_next_token(&t, file);
+
+                if (type == RIGHT_ROUNDED_BRACKET) {
+                        break;
+                }
+
+                if (type != COMMA) {
+                        printf("Missing comma\n");
+                        break;
+                }
         }
 
 }
