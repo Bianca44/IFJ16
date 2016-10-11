b7740a1f2273bd26337b2851b8096ac78bead0ca
tamarakrestiankova <xkrest07@stud.fit.vutbr.cz>
2016-10-11 19:45:02 +0200

    source code - quickSort algorithm

--- src/ial.c
+++ src/ial.c
@@ -1,9 +1,58 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
+void sort (char []);
+void quickSort (char[], int, int);
+int partition (char[], int, int);
 
 
-int main(int argc, char *argv[]) {
+void main() {
+    char a[] = "tamara";        
+	sort (a);
 
-        printf("argc= %d argv = %s\n", argc, argv[0]);
-        return 0;
+    printf("%s\n", a);
+}
+
+void sort (char a[]) {
+
+    quickSort (a, 0, (strlen(a)-1));        
+}
+
+void quickSort( char a[], int left, int right) {
+
+    int j;
+
+    if(left < right ) {
+
+        j = partition( a, left, right);
+        quickSort( a, left, j-1);
+        quickSort( a, j+1, right);
+    }
+	
+}
+
+int partition(char a[], int left, int right) {
+
+    int pivot, i, j, t;
+    pivot = a[left];
+    i = left; 
+    j = right+1;
+		
+    while(1) {
+
+   	    do ++i; while(a[i] <= pivot && i <= right);
+   	    do --j; while(a[j] > pivot);
+   	    if(i >= j) 
+            break;
+   	    
+        t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+   
+    t = a[left]; 
+    a[left] = a[j]; 
+    a[j] = t;
+    
+    return j;
 }
