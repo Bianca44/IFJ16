cc8daeca9f07055fb3f98d7e6ab705bc886b622a
Dávid Bolvanský <xbolva00@stud.fit.vutbr.cz>
2016-11-21 14:48:02 +0100

    Test to find bug in string concat

---
+++ src/test14.java
@@ -0,0 +1,13 @@
+class Main
+{
+static void run()
+{
+        int a;
+        ifj16.print("Zadejte cislo pro vypocet faktorialu: ");
+        a = ifj16.readInt();
+        int vysl;
+        vysl = 1;
+        vysl = vysl * a;
+        ifj16.print("Vysledek je: " + vysl + "\n");
+}
+}
