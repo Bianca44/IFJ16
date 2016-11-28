74dfa0e950b161a7b14198846dcf6a6c5b09b3e9
tamarakrestiankova <xkrest07@stud.fit.vutbr.cz>
2016-11-28 16:03:05 +0100

    pridane komentare k hlavickovym suborom

--- src/builtin.h
+++ src/builtin.h
@@ -2,12 +2,12 @@
 #define BUILTIN_H
 #include "symbol_table.h"
 
-void sort(char *str);
-int find(char *p, char *t);
-void print(tVar * var);
-int length(char *string);
-int compare(char *s1, char *s2);
-char *substr(char *s, int i, int n);
+void sort(char *str); //Quicksort
+int find(char *p, char *t); //Boyer-Moore algorithm
+void print(tVar * var); //prints values
+int length(char *string); //returns the lenght of a string
+int compare(char *s1, char *s2); //compares two strings
+char *substr(char *s, int i, int n); //finds a substring of a given string
 int read_int();
 double read_double();
 char *read_string();
